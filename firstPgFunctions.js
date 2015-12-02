b1669afa562ea7f9361c8eaa8d3f2f25080fe2f9
Keleigh <kzolnier@umich.edu>
2015-12-02 10:41:43 -0500

    changes

--- firstPgFunctions.js
+++ firstPgFunctions.js
@@ -1,10 +1,10 @@
 var userChannel = 'user_channel5';
+var colorChosen = 'none';
 var myUUID = PUBNUB.uuid();
 var pubnub = PUBNUB({
 	subscribe_key: 'sub-c-b1b8b6c8-8b1c-11e5-84ee-0619f8945a4f', // always required
 	publish_key: 'pub-c-178c8c90-9ea7-46e3-8982-32615dadbba0',    // only required if publishing
-	uuid: myUUID,
-	heartbeat: 60
+	uuid: myUUID
 });
 
 document.getElementById("letsplay").onclick = function () {
@@ -17,18 +17,53 @@ document.getElementById("letsplay").onclick = function () {
 };
 
 document.getElementById("namesubmit").onclick = function () {
-    if(document.getElementById(myUUID) == null){
+    if(colorChosen == 'none'){
+    	if(document.getElementById("username").value == '')
+    		alert("Please enter your name and select a color for your marker.");
+    	else
+    		alert("Please select a color for your marker.");
+    }
+    else if(document.getElementById("username").value == '')
+    	alert("Please enter your name.");
+    else if(document.getElementById(myUUID) == null){
 		pubnub.publish({
 					    channel: userChannel,        
-					    message: {entryNumber: myUUID, entryName: document.getElementById("username").value},
+					    message: {entryNumber: myUUID, entryName: document.getElementById("username").value, personColor: colorChosen},
 					    callback : function(m){},
 					    error: function(e){console.log(e)}
-					});
+		});
+		if(colorChosen != 'greenPerson')
+			document.getElementById('greenPerson').remove();
+		if(colorChosen != 'yellowPerson')
+			document.getElementById('yellowPerson').remove();
+		if(colorChosen != 'pinkPerson')
+			document.getElementById('pinkPerson').remove();
+		if(colorChosen != 'grayPerson')
+			document.getElementById('grayPerson').remove();
+		if(colorChosen != 'navyPerson')
+			document.getElementById('navyPerson').remove();
+		if(colorChosen != 'bluePerson')
+			document.getElementById('bluePerson').remove();
+		if(colorChosen != 'redPerson')
+			document.getElementById('redPerson').remove();
+		if(colorChosen != 'purplePerson')
+			document.getElementById('purplePerson').remove();
+		if(document.getElementById("playNameTxt") == null)
+			console.log("problem found");
+		document.getElementById("playNameTxt").innerHTML 
+					= document.getElementById("username").value;
+		document.getElementById("playNameTxt").style.fontSize = '40px';
+		document.getElementById("username").remove();
+		document.getElementById("namesubmit").remove();
+		document.getElementById(colorChosen).style.width = '100%';
+
+
 	}
 	else
-		alert ("you already submitted your name");
+		alert ("You have already submitted your name and color.");
 
 };
+
 
 function addName(usernumber, usernameIN){
 	var list = document.getElementById('demo');
@@ -38,6 +73,12 @@ function addName(usernumber, usernameIN){
 	list.appendChild(entry);
 }
 
+function highlightColor(color){
+	if(colorChosen != 'none')
+		document.getElementById(colorChosen).style.width='25%';
+	colorChosen = color;
+	document.getElementById(color).style.width='35%'; 
+}
 
 $(document).ready(function() {
 
@@ -61,23 +102,22 @@ $(document).ready(function() {
 						    error: function(e){console.log(e)}
 					});
 				}
-
 			}
 		},
 		connect: function(){console.log("connected")},
 		disconnect: function(){console.log("disconnected")},
 		callback: function(message, envelope, channel){
 			console.log(message);
-			if(message.entryNumber != null && message.entryName != null)
+			if(message.entryNumber != null && message.entryName != null && message.personColor != null){
 				addName(message.entryNumber, message.entryName);
+				if( document.getElementById(message.personColor) != null && message.entryNumber != myUUID)
+					document.getElementById(message.personColor).style.visibility = 'hidden';
+			}
 			else if (message.letsPlay != null)
 				location.href = "file:///Users/Keleigh/Desktop/493Site/node1.html";
 			else if (message.toRemove != null && document.getElementById(message.toRemove) != null){
 				document.getElementById(message.toRemove).remove();
 			}
-
-
-	
 		}
 	});
 
@@ -92,10 +132,6 @@ $(document).ready(function() {
 			}
 			else if(newArr[i].toRemove != null && document.getElementById(newArr[i].toRemove) != null){
 				document.getElementById(newArr[i].toRemove).remove();
-			}
-			else{
-				console.log(newArr[i]);
-				console.log("the above was not caught");
 			}
 		}
 
